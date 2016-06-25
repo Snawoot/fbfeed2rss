25a10914ec91d020f07373d6e52408f0e1f6b61e
Vladislav Yarmak <yarmak.vladislav@gmail.com>
2016-06-25 13:53:00 +0300

    test_user objects

--- fbgraph.py
+++ fbgraph.py
@@ -1,8 +1,9 @@
+import urllib
 import urllib2
 import json
 
-_feed_url = 'https://graph.facebook.com/%(ID)d/feed?access_token=%(access_token)s&limit=%(limit)d'
-_group_url = 'https://graph.facebook.com/%(ID)d?access_token=%(access_token)s'
+_feed_url = 'https://graph.facebook.com/v2.6/%(ID)d/feed?access_token=%(access_token)s&limit=%(limit)d'
+_group_url = 'https://graph.facebook.com/v2.6/%(ID)d?access_token=%(access_token)s'
 
 class FBGraph:
     def __init__(self, access_token, timeout=5):
@@ -10,6 +11,9 @@ class FBGraph:
         if isinstance(access_token, basestring):
             self._access_token = str(access_token)
             self._timeout = timeout
+
+            app_id, _, app_token = self._access_token.partition('|')
+            self._app_id = int(app_id) if app_token else None
         else:
             raise TypeError("Access token should be a string")
     
@@ -29,3 +33,14 @@ class FBGraph:
         } if continue_url is None else continue_url
         o = json.load(urllib2.urlopen(requrl, timeout = self._timeout))
         return o
+
+    def create_user(self):
+        if self.app_id is None:
+            raise ValueError('Unable to create user with Graph API instance initizized with page or user access token')
+        else:
+            return fbuser.FBUser(
+                self._access_token,
+                self._app_id,
+                self._timeout
+            )
+
---
+++ fbuser.py
@@ -0,0 +1,60 @@
+import urllib
+import urllib2
+import json
+
+_test_users_url = 'https://graph.facebook.com/v2.6/%(app_id)d/accounts/test-users?access_token=%(access_token)'
+_user_url = 'https://graph.facebook.com/v2.6/%(ID)d?access_token=%(access_token)s'
+
+_fb_url = 'https://facebook.com/'
+_fblogin_url = 'https://www.facebook.com/login.php?login_attempt=1'
+
+class FBUser:
+    def __init__(self, access_token, app_id, timeout=5):
+        """Create test user via FB Application API"""
+        self._timeout = timeout
+
+        if isinstance(access_token, basestring):
+            self._access_token = str(access_token)
+        else:
+            raise TypeError("Access token should be a string")
+
+        if isinstance(app_id, (int,long)):
+            self._app_id = app_id
+        else:
+            raise TypeError("App ID should be an integer number")
+
+        self.uid = None
+        self.uid, self.email, self.passwd = self._create_uid()
+        self.session = self._auth_session()
+
+    def _create_uid(self):
+        requrl = _test_users_url % {
+            'access_token': self._access_token,
+            'app_id': self._app_id
+        }
+        o = json.load(urllib2.urlopen(requrl, data='', timeout = self._timeout))
+        return o['id'], o['email'], o['password']
+
+    def _auth_session(self):
+        cj = CookieJar()
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+        opener.open(_fb_url).read()
+        formdata = urllib.urlencode(
+            {
+                'email': email,
+                'pass': passwd,
+                'persistent':'',
+                'default_persistent': 1
+            }
+        )
+        opener.open(_fblogin_url, formdata).read()
+        return cj
+
+    def fetch_url(self, args*):
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.session))
+        return opener.open(*args).read()
+
+    def __del__(self):
+        if self.uid is not None:
+            self._eliminate_uid()
+
